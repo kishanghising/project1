f4dd8d80d74ebe88ea8e9992450f21be48e1154c
Ashe-png <ashish.khatakho11@gmail.com>
2023-06-19 11:25:28 +0545

    changed routes

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,3 +1,6 @@
 const createOrUpdateNoteRoute = '/notes/new-note/';
 const clearPaymentRoute = '/notes/clear/';
 const notesRoute = '/notes/';
+const createRoute = '/notes/create/';
+const updateRoute = '/notes/update/';
+const historyRoute = '/notes/history/';
